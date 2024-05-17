CalyxOS/device_google_shusky

Add privapp perms for PixelDisplayService
Change-Id: I69792bbbd6844194f2098e12f3759dde805f6375

device-calyx.mk
@@ -1,5 +1,9 @@
 DEVICE_PACKAGE_OVERLAYS += device/google/shusky/overlay-calyx
 
+# Display
+PRODUCT_COPY_FILES += \
+    device/google/shusky/permissions/permissions_com.android.pixeldisplayservice.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/permissions_com.android.pixeldisplayservice.xml
+
 # EUICC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml \

nattolecats/device_google_akita

device-calyx.mk
@@ -1,5 +1,9 @@
 DEVICE_PACKAGE_OVERLAYS += device/google/akita/overlay-calyx
 
+# Display
+PRODUCT_COPY_FILES += \
+    device/google/akita/permissions/permissions_com.android.pixeldisplayservice.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/permissions_com.android.pixeldisplayservice.xml
+
 # EUICC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml \